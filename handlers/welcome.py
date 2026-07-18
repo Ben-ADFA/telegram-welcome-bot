b99c459fb25b246c944a1ec1545912039c042369
Ben-ADFA <benjohnson@appdevforall.org>
2026-07-18 11:45:01 -0400

    Update welcome.py

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -19,10 +19,12 @@ async def welcome_new_member(
         message = (
             f"👋 Welcome, {name}!\n\n"
             "Thanks for joining Code on the Go Discussions!\n\n"
-            "You can download Code on the Go here:\n"
+            "Feel free to introduce yourself and tell us why you're using Code on the Go.\n\n"
+            "If you haven't done so, you can download the app here:\n"
             "https://www.appdevforall.org/code-on-the-go/\n\n"
-            "Please note that our main support forum is at:\n"
-            "https://github.com/appdevforall/CodeOnTheGo/discussions"
+            "If you need help, please note that our main support forum is here:\n"
+            "https://github.com/appdevforall/CodeOnTheGo/discussions\n\n"
+            "We look forward to hearing from you!\n\n"
         )
 
         await update.message.reply_text(message)
