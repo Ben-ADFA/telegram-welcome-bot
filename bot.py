6c97fbb56599f721370d616a081d93fb46a72947
BenJgitFM <benjohnson@hoffmanpower.com>
2026-07-21 12:09:43 -0400

    Time update

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ def main():
 
     app.job_queue.run_daily(
         daily_welcome,
-        time=time(hour=9, minute=0, tzinfo=pacific)
+        time=time(hour=9, minute=30, tzinfo=pacific)
     )
 
     print("Community Bot is running...")
